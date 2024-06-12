16be6c30369fb412a2cf9c06a61f228c13b9f304
rabia <rabia.tuere@alumni.fh-aachen.de>
2024-06-12 15:13:31 +0200

    hi sofia

--- werbeseite/werbeseite.php
+++ werbeseite/werbeseite.php
@@ -62,6 +62,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "<span class='erfolgreich'>Anmeldung erfolgreich!</span>";
     }
 
+// Funktion zur Überprüfung des CSRF-Tokens
+//"Same-Origin-Policy", die sicherstellt, dass eine Webanwendung nur Anfragen an ihre eigene Domäne stellen kann und Anfragen von externen Quellen verhindert.
+//Cross-Site Request Forgery
+function verifyCsrfToken($token) {
+    return $token === $_SESSION['csrf_token'];
+}
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // CSRF-Token überprüfen
+    if (!isset($_POST['csrf_token']) || !verifyCsrfToken($_POST['csrf_token'])) {
+        $error = "Ungültiges CSRF-Token.";
+    } else {
+        // Überprüfung, ob alle erforderlichen Felder ausgefüllt wurden
+        if (empty($_POST["email"]) || empty($_POST["Vorname"]) || empty($_POST["Nachname"])) {
+            $error = "Bitte füllen Sie alle erforderlichen Felder aus.";
+        } elseif (!isset($_POST["datenschutz"])) {
+            $error = "Bitte stimmen Sie den Datenschutzbestimmungen zu.";
+        } elseif (!isValidEmail($_POST["email"])) {
+            $error = "Die eingegebene E-Mail-Adresse entspricht nicht den Vorgaben.";
+        } else {
+            // Anmeldung erfolgreich
+            $newsletterData = "E-Mail: " . htmlspecialchars($_POST['email']) . ", Anrede: " . htmlspecialchars($_POST['Anrede']) . ", Vorname: " . htmlspecialchars($_POST['Vorname']) . ", Nachname: " . htmlspecialchars($_POST['Nachname']) . ", Sprache Newsletter: " . htmlspecialchars($_POST['Newsletter_bitte_in']) . "\n";
+            saveNewsletterSignup($newsletterData);
+            echo "<span class='erfolgreich'>Anmeldung erfolgreich!</span>";
+        }
+    }
     // Anzeige der Fehlermeldung, falls vorhanden
     if ($error) {
         echo "<span class='error-message'>$error</span>";
@@ -177,7 +205,7 @@ $zahlen = mysqli_query($link,$sql_zahlen);
             margin: 0 auto;
             padding: 0;
             height: 100%;
-            text-align: center;
+            text-align: center;7
         }
         main {
             display: block;
